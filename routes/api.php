a5fa24f5502ab4aaeb12a1233c37c6223b8c6cdb
Alfian Arga Budiyanto <alvianarga1@gmail.com>
2020-05-17 03:16:04 +0000

    Pembuatan middleware CORS

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// Route::middleware('cors')->get('/notes','NoteController@all');
+// Route::middleware('cors')->get('/notes/{noteId}','NoteController@one');
+// Route::middleware('cors')->post('/notes','NoteController@store');
+// Route::middleware('cors')->put('/notes/{note}','NoteController@update');
+// Route::middleware('cors')->delete('/notes/{note}','NoteController@destroy');
+
+//Route lama, sebelum ada routemiddleware cors
 Route::get('/notes','NoteController@all');
 Route::get('/notes/{noteId}','NoteController@one');
 Route::post('/notes','NoteController@store');
